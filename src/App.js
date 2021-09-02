a3fa3285a9df2fe58b61faf241d69e415e6f192c
nguye374 <nguye374@sheridancollege.ca>
2021-09-02 03:26:55 -0400

    Sep 1st

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import React from 'react';
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
 const App = () => {
   const expenses = [
@@ -22,10 +24,15 @@ const App = () => {
       date: new Date(2021, 5, 12),
     },
   ];
+
+  const addExpenseHandler = expense => {
+    console.log('In App.js');
+    console.log(expense);
+  }
   return (
     <div>
-      <h2>Let's get started!</h2>
       <div>
+        <NewExpense onAddExpense={addExpenseHandler} />
         <Expenses item={expenses} />
       </div>
     </div>
